a8535cf3862565dc9050a215d716f59e6c38f142
Gerafp <gerardo.florespe@alumno.buap.mx>
2018-09-20 10:19:49 -0500

    Funcion para evitar bloqueos en los depositos
    Esta funcion sirve para evitar bloqueos en los depositos. Se elimina el archivo <b> pid.lck </b>

--- main.py
+++ main.py
@@ -11,21 +11,24 @@ import time
 import shlex
 
 
+def pid_lck(file):
+    if path.exists(file):
+        print("El archivo existe")
+        os.remove(file)
 
 def main():
-    print()
-
     env = os.environ.copy()
     retrieve_commstr = "/opt/archivematica/transfer-source-helpers/dspace-transfer-src-retrieve.py /etc/archivematica/automation-tools/dspace-transfer.src.conf"
     deposit_commstr ="/usr/lib/archivematica/automation-tools/transfers/transfer.py --transfer-type dspace \
-      --user uampruebas \
-      --api-key 835a29abce61490a588311215250c14b21ae845b \
-      --ss-user test \
-      --ss-api-key 38716374ae1dcdcf383b6a548314e71f7295b66b  \
-      --transfer-source 0662de16-377d-4406-928d-84318fe07660 \
+      --user (Usuario del DashBoard) \
+      --api-key (Key del usuario de DashBoard) \
+      --ss-user (Usuario del SS) \
+      --ss-api-key (Key del usuario del SS)  \
+      --transfer-source (UUI del transfer-source del SS) \
       --am-url http://localhost \
       --ss-url http://localhost:8000 "
     remove_commstr = "/opt/archivematica/transfer-source-helpers/dspace-transfer-src-delete.py /etc/archivematica/automation-tools/dspace-transfer.src.conf"
+    transfer_pid_file = "/usr/lib/archivematica/automation-tools/transfers/pid.lck" #Ubicacion por default
 
     state = subprocess.call(shlex.split(retrieve_commstr), env=env)
     flag = 0
@@ -36,13 +39,16 @@ def main():
         print("---------------------------------------------")
         state = subprocess.call(shlex.split(deposit_commstr), env=env)
         if state == 0:
+            #Eliminamos el archivo pid en caso de que exista
+            pid_lck(transfer_pid_file)
             i = 1
             print("---------------------------------------------")
             print("Nueva transferencia")
             print("Realizando ingesta a archivematica")
             print("---------------------------------------------")
             while i < 6 and state != 1:
-                #print(str(i*20) +" de 100%")
+                #Eliminamos el archivo pid en caso de que exista
+                pid_lck(transfer_pid_file)
                 time.sleep(30)
                 i += 1
                 state = subprocess.call(shlex.split(deposit_commstr), env=env)
